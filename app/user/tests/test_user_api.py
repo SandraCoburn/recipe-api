f43229e88d0879bf75c6b7f25c91f9cea206f25d
sandracoburn <sandra_coburn@hotmail.com>
2021-08-22 15:27:33 -0700

    Fix last text by adding name to the payload

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         '''Test that the password must be more thatn 5 characters'''
-        payload = {'email': 'test@gmail.com', 'password': 'pw'}
+        payload = {'email': 'test@gmail.com', 'password': 'pw', 'name': 'Test',}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
