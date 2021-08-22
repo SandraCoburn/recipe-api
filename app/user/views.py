eb5d6abba0688e1d78a1594c60744f49168c8e1e
sandracoburn <sandra_coburn@hotmail.com>
2021-08-22 11:28:02 -0700

    Add tests for create user API

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    '''Createa new user in the system'''
+    serializer_class = UserSerializer
