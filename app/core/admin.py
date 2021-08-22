fde2ab73c00c9434f146cb7d2953b475eaef75e3
sandracoburn <sandra_coburn@hotmail.com>
2021-08-22 08:07:20 -0700

    Add postgres support to Dockerfile

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,26 +4,27 @@ from django.utils.translation import gettext as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-       # Each bracket is a section
-      (None, {'fields': ('email', 'password')}), # top of page, first field in the form
-      (_('Personal Info'), {'fields': ('name',)}),
-      (
-        _('Permissions'),
-        {'fields': ('is_active', 'is_staff', 'is_superuser')}
-      ),
-      (_('Important dates'), {'fields': ('last_login',)}),
+        # Each bracket is a section
+        # top of page, first field in the form
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+          _('Permissions'),
+          {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     add_fieldsets = (
-      (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'password1', 'password2')
-      }),
+        (None, {
+          'classes': ('wide',),
+          'fields': ('email', 'password1', 'password2')
+        }),
     )
 
 
 admin.site.register(models.User, UserAdmin)
-
